22bfe3b7fe4d3866db45a5e0fc143f45d8276f20
Lerish-vZ <lerishavz@gmail.com>
2024-02-12 10:46:47 +0200

    Update

--- src/Lesson_2/lesson2.java
+++ src/Lesson_2/lesson2.java
@@ -1,6 +1,9 @@
 package Lesson_2;
 
-public class lesson2 { //
+public class lesson2 { //Class name in upper camel case
+    //Constants should be declared in all uppercase
+    //variable names are short and meaningfull in lower camel case
+    //Methods in lower camel case
     public static void main(String args[]) { //Command-line arguments are passed to the program through the args[] array.
         
     }

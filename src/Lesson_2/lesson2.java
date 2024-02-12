300903d05b29eb6a4723b551ee3da6092938fb99
Lerish-vZ <lerishavz@gmail.com>
2024-02-12 10:45:02 +0200

    Update

--- src/Lesson_2/lesson2.java
+++ src/Lesson_2/lesson2.java
@@ -1,7 +1,7 @@
 package Lesson_2;
 
-public class lesson2 {
-    public static void main(String args[]) {
+public class lesson2 { //
+    public static void main(String args[]) { //Command-line arguments are passed to the program through the args[] array.
         
     }
 }

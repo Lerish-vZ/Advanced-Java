20b2b5a47cf592a452d05e474c678ed8cfaa027d
Lerish-vZ <lerishavz@gmail.com>
2024-02-16 11:14:00 +0200

    Update

---
+++ src/Lesson_3/domain/Employee.java
@@ -0,0 +1,44 @@
+package Lesson_2.domain;
+
+public class Employee {
+
+    public int empId;
+    public String empName;
+    public String SSN;
+    public double empSalary;
+
+    public Employee() {
+
+    }
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public double getEmpSalary() {
+        return empSalary;
+    }
+
+    public void setEmpSalary(double empSalary) {
+        this.empSalary = empSalary;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+}

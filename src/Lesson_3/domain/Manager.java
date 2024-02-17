cef7ec911c7742cc31e412fd8ae3dae8f62883a6
Lerish-vZ <lerishavz@gmail.com>
2024-02-17 11:27:28 +0200

    Update

--- src/Lesson_3/domain/Manager.java
+++ src/Lesson_3/domain/Manager.java
@@ -1,2 +1,14 @@
-package Lesson_3.domain;public class Manager {
+package Lesson_3.domain;
+public class Manager extends Employee{
+    private String deptName;
+
+    public Manager(int empId, String name, String ssn, double salary, String deptName){
+        super(empId, name, ssn, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
 }
+

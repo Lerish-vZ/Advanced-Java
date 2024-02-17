0b2a2dee682aa3d2c2f188594aa5c2f0833bd71c
Lerish-vZ <lerishavz@gmail.com>
2024-02-17 11:34:25 +0200

    Update

--- src/Lesson_3/domain/Director.java
+++ src/Lesson_3/domain/Director.java
@@ -1,2 +1,15 @@
-package Lesson_3.domain;public class Director {
+package Lesson_3.domain;
+
+public class Director extends Manager{
+
+    private double budget;
+
+    public Director(int empId, String name, String ssn, double salary, String deptName, double budget){
+        super(empId, name, ssn, salary, deptName);
+        this.budget = budget;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
 }

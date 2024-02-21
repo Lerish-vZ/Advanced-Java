f79b3a3e2a7f3da4f6de3970f625a409f057fbbb
Lerish-vZ <lerishavz@gmail.com>
2024-02-21 08:50:18 +0200

    Update

--- src/Lesson_4_P1/business/EmployeeStockPlan.java
+++ src/Lesson_4_P1/business/EmployeeStockPlan.java
@@ -1,5 +1,9 @@
 package Lesson_4_P1.business;
 
 public class EmployeeStockPlan {
+    private final int employeeShares = 10;
+    private final int managerShares = 100;
+    private final int directorShares = 1000;
+
     
 }

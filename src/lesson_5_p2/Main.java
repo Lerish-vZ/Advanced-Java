d6fa7e47c1a33876178e28e7a95131098515f1ea
Lerish-vZ <lerishavz@gmail.com>
2024-02-26 11:24:42 +0200

    Update

--- src/lesson_5_p2/Main.java
+++ src/lesson_5_p2/Main.java
@@ -1,2 +1,10 @@
-package lesson_5_p2;public class Main {
+package lesson_5_p2;
+
+public class Main {
+    public static void Main(String args[]){
+        Employee jane = new Employee("Jane Doe", "Manager", "HR", 65000);
+        Employee john = new Employee("John Doe", "Staff", "HR", 55000);
+        System.out.println("Jane's withholding: " + jane.getWithholding());
+        System.out.println("John's bonus " + john.getBonus());
+    }
 }

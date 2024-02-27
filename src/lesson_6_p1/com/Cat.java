5b59bca8e212ced23d5d327daaa5e8a410ff269e
Lerish-vZ <lerishavz@gmail.com>
2024-02-27 12:43:59 +0200

    Update

--- src/lesson_6_p1/com/Cat.java
+++ src/lesson_6_p1/com/Cat.java
@@ -1,4 +1,34 @@
 package lesson_6_p1.com;
 
-public class Cat {
+public class Cat extends Animal implements Pet {
+    private String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat(String name){
+        super(4);
+        this.name = name;
+    }
+
+    public Cat(){
+        this("Fluffy");
+    }
+
+    @Override
+    public void play(){
+        System.out.println(name + " likes to play with string.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Cats like to eat spiders and fish.");
+    }
 }

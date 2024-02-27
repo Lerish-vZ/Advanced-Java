fab6126a350962bc8e99ef1d37b24b89a2c3d190
Lerish-vZ <lerishavz@gmail.com>
2024-02-27 09:28:14 +0200

    Update

--- src/lesson_5_p3/com/Branch.java
+++ src/lesson_5_p3/com/Branch.java
@@ -1,4 +1,14 @@
 package lesson_5_p3.com;
 
 public enum Branch {
+    LA("Basic"), BOSTON("Loan"), BANGALORE("Full"), MUMBAI("Full");
+
+    String serviceLevel;
+    private Branch(String serviceLevel) {
+        this.serviceLevel = serviceLevel;
+    }
+
+    public String getServiceLevel() {
+        return serviceLevel;
+    }
 }

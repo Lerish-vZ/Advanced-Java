61838f7e4660d449a3005920d01ed9ff61e6b54d
Lerish-vZ <lerishavz@gmail.com>
2024-02-27 12:53:02 +0200

    Update

--- src/lesson_6_p1/com/PetMain.java
+++ src/lesson_6_p1/com/PetMain.java
@@ -12,10 +12,31 @@ public class PetMain {
         a = new Spider();
         a.eat();
         a.walk();
+
+        Pet p;
+
+        Cat c = new Cat("Tom");
+        c.eat();
+        c.walk();
+        c.play();
+        a = new Cat();
+        a.eat();
+        a.walk();
+        p = new Cat();
+        p.setName("Mr. Whiskers");
+        p.play();
+
+        Fish f = new Fish();
+        f.setName("Guppy");
+        f.eat();
+        f.walk();
+        f.play();
+        a = new Fish();
+        a.eat();
+        a.walk();
     }
     
     public static void playWithAnimal(Animal a) {
         // completed in practice
     }
-    
 }

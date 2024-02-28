541d5fa1aba92ec2e528ed2ab57ee44b614069d4
Lerish-vZ <lerishavz@gmail.com>
2024-02-28 09:13:29 +0200

    Update

--- src/lesson_6_p2/com/BankOperations.java
+++ src/lesson_6_p2/com/BankOperations.java
@@ -2,19 +2,9 @@ package lesson_6_p2.com;
 
 public interface BankOperations {
 
-    public void addCustomer(String f, String l,Branch b) {
-        int i = numberOfCustomers++;
-        customers[i] = new Customer(f, l,b);
-    }
-
-    public int getNumOfCustomers() {
-        return numberOfCustomers;
-    }
-
-    public Customer getCustomer(int customerIndex) {
-        return customers[customerIndex];
-    }
-
+    public void addCustomer(String f, String l,Branch b);
+    public int getNumOfCustomers();
+    public Customer getCustomer(int customerIndex);
 
     public default void generateReport() {
 

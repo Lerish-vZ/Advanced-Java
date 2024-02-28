05e922804358bac5ee02d661e14accd546f014db
Lerish-vZ <lerishavz@gmail.com>
2024-02-28 09:01:35 +0200

    Update

--- src/lesson_6_p2/com/BankOperations.java
+++ src/lesson_6_p2/com/BankOperations.java
@@ -1,5 +1,17 @@
 package lesson_6_p2.com;
 
 public interface BankOperations {
-  
+
+    public void addCustomer(String f, String l,Branch b) {
+        int i = numberOfCustomers++;
+        customers[i] = new Customer(f, l,b);
+    }
+
+    public int getNumOfCustomers() {
+        return numberOfCustomers;
+    }
+
+    public Customer getCustomer(int customerIndex) {
+        return customers[customerIndex];
+    }
 }

a89de4620df94ccab514f627b9c900b6c6de6d04
Lerish-vZ <lerishavz@gmail.com>
2024-06-03 11:31:06 +0200

    update

--- src/lesson_9_p3/com/lambda/SupplierTest.java
+++ src/lesson_9_p3/com/lambda/SupplierTest.java
@@ -21,6 +21,24 @@ public class SupplierTest {
     eList.stream().forEach(Employee::printSummary);
     
     // Add new employee here
+    eList.add(
+            new Employee.Builder()
+                    .givenName("Jill")
+                    .surName("Doe")
+                    .age(26)
+                    .gender(Gender.FEMALE)
+                    .role(Role.STAFF)
+                    .dept("Sales")
+                    .startDate(LocalDate.of(2012, 7, 14))
+                    .salary(45000)
+                    .email("jill.doe@example.com")
+                    .phoneNumber("202-123-4678")
+                    .address("33 3rd St")
+                    .city("Smallville")
+                    .state("KS")
+                    .code("12333")
+                    .build()
+    );
     
     System.out.println("\n=== Print employee list after");
     eList.stream().forEach(Employee::printSummary);

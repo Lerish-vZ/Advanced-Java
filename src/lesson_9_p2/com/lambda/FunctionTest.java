a65a29001876be6faf10cde62bbf1c847864f6a4
Lerish-vZ <lerishavz@gmail.com>
2024-06-03 11:23:44 +0200

    update

--- src/lesson_9_p2/com/lambda/FunctionTest.java
+++ src/lesson_9_p2/com/lambda/FunctionTest.java
@@ -14,7 +14,8 @@ public class FunctionTest {
     List<Employee> eList = Employee.createShortList();
     Employee first = eList.get(0);
     
-    ToDoubleFunction<Employee> eFunc; // Write your function lambda here
+    ToDoubleFunction<Employee> eFunc =
+            e -> e.getSalary() * Bonus.byRole(e.getRole()); // Write your function lambda here
       
     System.out.println("=== First Employee Bonus");
     first.printSummary();

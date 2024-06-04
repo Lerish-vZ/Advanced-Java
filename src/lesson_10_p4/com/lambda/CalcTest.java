4e5f0cf809c9b1af8cf35e9e0afd979edadade13
Lerish-vZ <lerishavz@gmail.com>
2024-06-04 14:04:09 +0200

    update

--- src/lesson_10_p4/com/lambda/CalcTest.java
+++ src/lesson_10_p4/com/lambda/CalcTest.java
@@ -21,16 +21,23 @@ public class CalcTest {
         double t1 = tList.stream()
                         .filter(t -> t.getBuyerName().equals("Radio Hut"))
                                 .mapToDouble(t -> t.getTransactionTotal())
+                                        .sum();
 
         
         System.out.printf("Radio Hut Total: $%,9.2f%n", t1);
         
         // Replace with Stream
-        double t2 = 0;
+        double t2 = tList.stream()
+                .filter(t -> t.getBuyerName().equals("PriceCo"))
+                .mapToDouble(t -> t.getTransactionTotal())
+                .sum();
         
         System.out.printf("PriceCo Total: $%,9.2f%n", t2);
         // Replace with stream
-        double t3 = 0;
+        double t3 = tList.stream()
+                .filter(t -> t.getBuyerName().equals("Radio Hut"))
+                .mapToDouble(t -> t.getTransactionTotal())
+                .sum();;
         
         System.out.printf("Best Deals Total: $%,9.2f%n", t3);
         

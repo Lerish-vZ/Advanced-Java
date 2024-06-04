131ab1a6e70fc0846d325636c0b5da28dd5ea3b3
Lerish-vZ <lerishavz@gmail.com>
2024-06-04 12:52:37 +0200

    update

--- src/lesson_10_p1/com/lambda/PeekTest.java
+++ src/lesson_10_p1/com/lambda/PeekTest.java
@@ -24,6 +24,7 @@ public class PeekTest {
         tList.stream()
                 .filter(t -> t.getState().equals(State.CA))
                 .filter(t -> t.getProduct().equals("Widget Pro"))
+                .peek(taxReport)
                 .map(t -> t.getTransactionTotal() * TaxRate.byState(t.getState()))
                 .forEach(amt -> System.out.printf("Txn tax: $%,9.2f%n", amt));
         

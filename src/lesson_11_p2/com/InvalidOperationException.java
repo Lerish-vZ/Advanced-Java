041c7bf0ab7af58776d6b18ed815a7b895854b03
Lerish-vZ <lerishavz@gmail.com>
2024-06-07 11:33:27 +0200

    update

--- src/lesson_11_p2/com/InvalidOperationException.java
+++ src/lesson_11_p2/com/InvalidOperationException.java
@@ -12,4 +12,8 @@ public class InvalidOperationException extends Exception {
     public InvalidOperationException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public InvalidOperationException(Throwable cause) {
+        super(cause);
+    }
 }

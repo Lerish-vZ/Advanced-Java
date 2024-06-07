5613cacb0b57c7353fc93bc5f8c346ce4bfca9ee
Lerish-vZ <lerishavz@gmail.com>
2024-06-07 10:33:45 +0200

    update

--- src/lesson_11_p1/com/ExceptionMain.java
+++ src/lesson_11_p1/com/ExceptionMain.java
@@ -1,7 +1,9 @@
 package lesson_11_p1.com;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ExceptionMain {
     public static void main(String[] args) {
@@ -12,10 +14,18 @@ public class ExceptionMain {
             System.exit(1);
         }
 
-        BufferedReader b = new BufferedReader(new FileReader(args[0]));
-        String s = null;
-        while((s = b.readLine()) != null){
-            System.out.println(s);
-        }
+
+       try(BufferedReader b = new BufferedReader(new FileReader(args[0]));){
+           String s = null;
+           while((s = b.readLine()) != null){
+               System.out.println(s);
+           }
+        } catch (FileNotFoundException e) {
+           System.out.println("File not found: " + args[0]);
+           System.exit(1);
+       } catch (IOException e) {
+           System.out.println("Error reading file: " + e.getMessage());
+           System.exit(1);
+       }
     }
 }

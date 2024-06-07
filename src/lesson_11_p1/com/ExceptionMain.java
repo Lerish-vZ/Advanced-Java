dec7f19d0328d3a0b52870e57fceafdb30f96269
Lerish-vZ <lerishavz@gmail.com>
2024-06-07 10:02:33 +0200

    update

--- src/lesson_11_p1/com/ExceptionMain.java
+++ src/lesson_11_p1/com/ExceptionMain.java
@@ -6,7 +6,7 @@ public class ExceptionMain {
             System.out.println("Reading from file: " + args[0]);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("No file specified, quitting!");
-            
+            System.exit(1);
         }
     }
 }

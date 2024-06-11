3f6d6a76c6e5e2e7709a2c77bf998a912f92f3c9
Lerish-vZ <lerishavz@gmail.com>
2024-06-11 12:14:23 +0200

    update

--- src/lesson_12_p2/com/DepartArrive.java
+++ src/lesson_12_p2/com/DepartArrive.java
@@ -38,7 +38,14 @@ public class DepartArrive {
     // The flight time is 22 hours
     //   Will the traveler make a meeting in Bangalore Monday at 9 AM local time?
     //   Can the traveler call her husband at a reasonable time when she arrives?
-
+    departure = LocalDateTime.of(2014, JUNE, 28, 22, 30);
+    departSFO = ZonedDateTime.of(departure, SFO);
+    System.out.println("Flight 456 leaves SFO at: " + departSFO);
+    ZonedDateTime departTimeAtBLR = departSFO.toOffsetDateTime().atZoneSameInstant(BLR);
+    System.out.println("Local time BLR at departure: " + departTimeAtBLR);
+    System.out.println("Flight time: 22 hours");
+    ZonedDateTime arriveBLR = departSFO.plusHours(22).toOffsetDateTime().atZoneSameInstant(BLR);
+    System.out.println("Flight 456 arrives BLR: " + arriveBLR);
 
     // Flight 123, San Francisco to Boston, leaves SFO at 10:30 PM Saturday, November 1st, 2014
     // Flight time is 5 hours 30 minutes.

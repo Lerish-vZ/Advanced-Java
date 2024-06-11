c57671902eceb1ed51b62ce27c36202c4ca1a7be
Lerish-vZ <lerishavz@gmail.com>
2024-06-11 12:27:44 +0200

    update

--- src/lesson_12_p3/com/TimeBetween.java
+++ src/lesson_12_p3/com/TimeBetween.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class TimeBetween {
@@ -11,16 +12,19 @@ public class TimeBetween {
   public static void main(String[] args) {
     // Create a formatter to accept date entries using the USA common standard,
     // month day, year = for example, April 8, 1970
+    String dateFormat = "MMMM d, yyyy";
+    LocalDate aDate = null;
 
     boolean validStr = false;
     
     // Create a formatter
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     while (!validStr) {
       System.out.print("Enter a date: ");
       try {
         String dateEntered = br.readLine();
-        
+        aDate = LocalDate.parse(dateEntered, formatter);
         // Use the parse method with the formatter to create a date
         validStr = true;
       } catch (IOException | DateTimeParseException ex) {

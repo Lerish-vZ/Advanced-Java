5ede789db60e9470cb5229c96ac3cb99ac691e83
Lerish-vZ <lerishavz@gmail.com>
2024-06-11 12:30:13 +0200

    update

--- src/lesson_12_p3/com/TimeBetween.java
+++ src/lesson_12_p3/com/TimeBetween.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
@@ -21,7 +22,7 @@ public class TimeBetween {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     while (!validStr) {
-      System.out.print("Enter a date: ");
+      System.out.print("Enter a date: (" + dateFormat + "): ");
       try {
         String dateEntered = br.readLine();
         aDate = LocalDate.parse(dateEntered, formatter);
@@ -35,13 +36,18 @@ public class TimeBetween {
     // Print the date out
     System.out.println("Date entered was: ");
     LocalDate now = LocalDate.now();
-    
+    Period between;
+    if(aDate.isBefore(now)) {
+      between = Period.between(aDate, now);
+    } else {
+      between = Period.between(now, aDate);
+    }
     // Calculate the years, months and days between now and the date entered
 
     // Print out the 
-    int years = 0;
-    int months = 0;
-    int days = 0;
+    int years = between.getYears();
+    int months = between.getMonths();
+    int days = between.getDays();
     System.out.println("There are " + years + " years, " 
             + months + " months, " 
             + days + " days between now and the date entered.");

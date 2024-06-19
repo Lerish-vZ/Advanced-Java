d3effc9486e5550aa328147d54577c538fcb62de
Lerish-vZ <lerishavz@gmail.com>
2024-06-19 12:45:33 +0200

    update

--- src/lesson_14_p2/com/lambda/DirFind.java
+++ src/lesson_14_p2/com/lambda/DirFind.java
@@ -8,22 +8,21 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 /**
- *
  * @author oracle
  */
 public class DirFind {
-    
+
     public static void main(String[] args) {
 
-        try(Stream<Path> files = Files.find(Paths.get("."), 9, (p,a) -> a.isDirectory())){ // Add try with resources
-            
+        try (Stream<Path> files = Files.find(Paths.get("."), 9, (p, a) -> a.isDirectory())) { // Add try with resources
+
             System.out.println("\n=== Find all dirs ===");
             // Print out directory list here
             files.forEach(line -> System.out.println(line));
-                
-        }catch (IOException e){
+
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
 
-    } 
+    }
 }

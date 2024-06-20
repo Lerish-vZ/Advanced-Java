97a8ab7c0c43c49eaae6d4ee53d17a362e73e8e3
Lerish-vZ <lerishavz@gmail.com>
2024-06-20 13:43:57 +0200

    update

--- src/lesson_15_p2/com/client/NetworkClientCallable.java
+++ src/lesson_15_p2/com/client/NetworkClientCallable.java
@@ -1,5 +1,7 @@
 package lesson_15_p2.com.client;
 
+import com.sun.net.httpserver.Request;
+
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
@@ -7,6 +9,20 @@ import java.util.concurrent.Callable;
 
 
 public class NetworkClientCallable implements Callable<RequestResponse> {
+    private final RequestResponse lookup;
 
+    public NetworkClientCallable(RequestResponse lookup) {
+        this.lookup = lookup;
+    }
+
+
+    @Override
+    public RequestResponse call() throws Exception {
+        try(Socket sock = new Socket(lookup.host, lookup.port);
+                Scanner scanner = new Scanner(sock.getInputStream())) {
+            lookup.response = scanner.next();
+            return lookup;
+        }
+    }
 
 }

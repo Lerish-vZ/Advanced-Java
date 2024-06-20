f6c3fea0ebcabb2fa09ffa6a85027e509467f949
Lerish-vZ <lerishavz@gmail.com>
2024-06-20 13:12:43 +0200

    update

--- src/lesson_15_p1/com/CountRunnable.java
+++ src/lesson_15_p1/com/CountRunnable.java
@@ -2,10 +2,14 @@ package lesson_15_p1.com;
 
 public class CountRunnable implements Runnable{
   
-   //final int count;
-  //final String threadName;
+   final int count;
+   final String threadName;
   
   // Setup Constructor to initialize variables
+  public CountRunnable(int count, String name){
+    this.count = count;
+    this.threadName = name;
+  }
   
   public void run(){ 
     // Count with for loop

61520e6e4f5e532919b24aed83f7e5c602918d5e
Lerish-vZ <lerishavz@gmail.com>
2024-06-21 12:05:31 +0200

    update

--- src/lesson_17_p1/com/lambda/CalcTest.java
+++ src/lesson_17_p1/com/lambda/CalcTest.java
@@ -7,24 +7,26 @@ import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
 /**
- *
  * @author oracle
  */
 public class CalcTest {
-    
+
     public static void main(String[] args) {
 
         List<SalesTxn> tList = SalesTxn.createTxnList();
-        
+
         // Print out Transaction Totals
         System.out.println("=== Transactions Totals ===");
-        
+
         // Put code here
-        
-        
+        Stream<SalesTxn> s1 = tList.stream();
+
+        Stream<SalesTxn> s2 = s1.filter(e -> e.getBuyerName().equalsIgnoreCase("Radio Hut"));
+
+
         double t1 = 0; // Put result here
-        
+
         System.out.printf("Radio Hut Total: $%,9.2f%n", t1);
-        
+
     }
 }

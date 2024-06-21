cc8b89e56c9f9b6deca040f200be1d13869fbe1a
Lerish-vZ <lerishavz@gmail.com>
2024-06-21 09:26:26 +0200

    update

--- src/lesson_16_p1/com/RandomArrayAction.java
+++ src/lesson_16_p1/com/RandomArrayAction.java
@@ -1,4 +1,15 @@
 package lesson_16_p1.com;
 
 public class RandomArrayAction {
+    private final int threshold;
+    private final int[] myArray;
+    private int start;
+    private int end;
+
+    public RandomArrayAction(int[] myArray, int start, int end, int threshold) {
+        this.myArray = myArray;
+        this.start = start;
+        this.end = end;
+        this.threshold = threshold;
+    }
 }

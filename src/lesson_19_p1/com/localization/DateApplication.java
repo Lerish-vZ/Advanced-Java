f0461a0ee64730832a2e47ae2319b656008c21d3
Lerish-vZ <lerishavz@gmail.com>
2024-06-24 14:49:42 +0200

    update

--- src/lesson_19_p1/com/localization/DateApplication.java
+++ src/lesson_19_p1/com/localization/DateApplication.java
@@ -12,84 +12,96 @@ import java.util.ResourceBundle;
 
 public class DateApplication {
     PrintWriter pw = new PrintWriter(System.out, true);
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     Locale ruLocale = new Locale("ru", "RU");
-    Locale currentLocale = Locale.US;     
+    Locale currentLocale = Locale.US;
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", Locale.US);
-         
+
     LocalDateTime today = LocalDateTime.now();
     DateTimeFormatter df;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         DateApplication dateApp = new DateApplication();
-        dateApp.run();        
+        dateApp.run();
     }
-    
-    public void run(){
+
+    public void run() {
         String line = "";
-        
-        while (!(line.equals("q"))){
+
+        while (!(line.equals("q"))) {
             this.printMenu();
-            try { line = this.br.readLine(); } 
-            catch (IOException e){ 
-              System.out.println("Error: " + e.getMessage()); }
-            
-            switch (line){
-                case "1": this.setEnglish(); break;
-                case "2": this.setFrench(); break;
-                case "3": this.setChinese(); break;
-                case "4": this.setRussian(); break;
+            try {
+                line = this.br.readLine();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
             }
-        }       
+
+            switch (line) {
+                case "1":
+                    this.setEnglish();
+                    break;
+                case "2":
+                    this.setFrench();
+                    break;
+                case "3":
+                    this.setChinese();
+                    break;
+                case "4":
+                    this.setRussian();
+                    break;
+            }
+        }
     }
-    
-    public void printMenu(){
-      pw.println("=== Date App ===");
-      
-      // Full Date
-      df = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(currentLocale);
-      pw.println(messages.getString("date1") + " " + today.format(df));
 
-      // Long Date
-      
-      // Short Date
-      
-      // Medium Date/Time
+    public void printMenu() {
+        pw.println("=== Date App ===");
 
-      // Medium Time
-      
+        // Full Date
+        df = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(currentLocale);
+        pw.println(messages.getString("date1") + " " + today.format(df));
+
+        // Long Date
+        df = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(currentLocale);
+        pw.println(messages.getString("date2") + " " + today.format(df));
+        
+        // Short Date
+
+        // Medium Date/Time
+
+        // Medium Time
+
         pw.println("\n--- Choose Language Option ---");
         pw.println("1. " + messages.getString("menu1"));
         pw.println("2. " + messages.getString("menu2"));
         pw.println("3. " + messages.getString("menu3"));
         pw.println("4. " + messages.getString("menu4"));
         pw.println("q. " + messages.getString("menuq"));
-        System.out.print(messages.getString("menucommand") + " ");        
+        System.out.print(messages.getString("menucommand") + " ");
     }
-    
-    public void setEnglish(){
+
+    public void setEnglish() {
         // Set currentLocale
         currentLocale = Locale.US;
         // Set messages
         messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
     }
-    
-    public void setFrench(){
+
+    public void setFrench() {
         // Set currentLocale
         currentLocale = Locale.FRANCE;
         // Set messages
         messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
     }
-    
-    public void setChinese(){
+
+    public void setChinese() {
         // Set currentLocale
         currentLocale = Locale.CHINA;
         // Set messages
         messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
     }
-    
-    public void setRussian(){
+
+    public void setRussian() {
         // Set currentLocale
         currentLocale = ruLocale;
         // Set messages
